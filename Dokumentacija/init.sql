a1ac6195005d0a81d4be451b4b75aff2b22fd112
Adnan Kičin <adnankicin92@gmail.com>
2016-04-27 09:03:04 +0200

    init.sql v2

--- Dokumentacija/init.sql
+++ Dokumentacija/init.sql
@@ -7,6 +7,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 -- -----------------------------------------------------
 -- Schema mydb
 -- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `mydb` ;
 
 -- -----------------------------------------------------
 -- Schema mydb
@@ -15,24 +16,51 @@ CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 COLLATE utf8_gener
 USE `mydb` ;
 
 -- -----------------------------------------------------
+-- Table `mydb`.`Grupe`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `mydb`.`Grupe` ;
+
+CREATE TABLE IF NOT EXISTS `mydb`.`Grupe` (
+  `idGrupe` INT NOT NULL COMMENT '',
+  `naziv` VARCHAR(45) NULL COMMENT '',
+  `RedniBroj` INT(11) NULL COMMENT '',
+  `Kapacitet` INT(11) NULL COMMENT '',
+  PRIMARY KEY (`idGrupe`)  COMMENT '')
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Table `mydb`.`Djeca`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `mydb`.`Djeca` ;
+
 CREATE TABLE IF NOT EXISTS `mydb`.`Djeca` (
   `idDjeca` INT NOT NULL COMMENT '',
-  `Ime` VARCHAR(45) NULL COMMENT '',
-  `Prezime` VARCHAR(45) NULL COMMENT '',
-  `DatumRodjenja` VARCHAR(45) NULL COMMENT '',
-  `AdresaPrebivalista` VARCHAR(45) NULL COMMENT '',
-  `ImenaRoditelja` VARCHAR(45) NULL COMMENT '',
-  `BrojTelefona` VARCHAR(45) NULL COMMENT '',
-  `Djecacol` VARCHAR(45) NULL COMMENT '',
-  PRIMARY KEY (`idDjeca`)  COMMENT '')
+  `Ime` VARCHAR(255) NULL COMMENT '',
+  `Prezime` VARCHAR(255) NULL COMMENT '',
+  `DatumRodjenja` VARCHAR(255) NULL COMMENT '',
+  `AdresaPrebivalista` VARCHAR(255) NULL COMMENT '',
+  `ImeRoditelja` VARCHAR(255) NULL COMMENT '',
+  `BrojTelefona` VARCHAR(255) NULL COMMENT '',
+  `PrezimeRoditelja` VARCHAR(255) NULL COMMENT '',
+  `DatumUpisa` DATE NULL COMMENT '',
+  `DatumIsteka` DATE NULL COMMENT '',
+  `idGrupe` INT NULL COMMENT '',
+  PRIMARY KEY (`idDjeca`)  COMMENT '',
+  INDEX `fk_grupe_idx` (`idGrupe` ASC)  COMMENT '',
+  CONSTRAINT `fk_grupe`
+    FOREIGN KEY (`idGrupe`)
+    REFERENCES `mydb`.`Grupe` (`idGrupe`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`Korisnici`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `mydb`.`Korisnici` ;
+
 CREATE TABLE IF NOT EXISTS `mydb`.`Korisnici` (
   `idKorisnika` INT NOT NULL COMMENT '',
   `Ime` VARCHAR(255) NULL COMMENT '',
@@ -48,6 +76,8 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `mydb`.`Uplate`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `mydb`.`Uplate` ;
+
 CREATE TABLE IF NOT EXISTS `mydb`.`Uplate` (
   `idUplate` INT NOT NULL COMMENT '',
   `DatumUplate` DATE NULL COMMENT '',
@@ -65,20 +95,10 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Grupe`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Grupe` (
-  `idGrupe` INT NOT NULL COMMENT '',
-  `naziv` VARCHAR(45) NULL COMMENT '',
-  `RedniBroj` INT(11) NULL COMMENT '',
-  `Kapacitet` INT(11) NULL COMMENT '',
-  PRIMARY KEY (`idGrupe`)  COMMENT '')
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `mydb`.`Vaspitaci`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `mydb`.`Vaspitaci` ;
+
 CREATE TABLE IF NOT EXISTS `mydb`.`Vaspitaci` (
   `idVaspitaci` INT NOT NULL COMMENT '',
   `Ime` VARCHAR(255) NULL COMMENT '',
@@ -99,6 +119,8 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `mydb`.`Aktivnosti`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `mydb`.`Aktivnosti` ;
+
 CREATE TABLE IF NOT EXISTS `mydb`.`Aktivnosti` (
   `idAktivnosti` INT NOT NULL COMMENT '',
   `Naziv` VARCHAR(255) NULL COMMENT '',
@@ -111,18 +133,21 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `mydb`.`AktivnostiDjeca`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `mydb`.`AktivnostiDjeca` ;
+
 CREATE TABLE IF NOT EXISTS `mydb`.`AktivnostiDjeca` (
   `idDjeca` INT NOT NULL COMMENT '',
   `idAktivnosti` INT NOT NULL COMMENT '',
-  INDEX `fk_Aktivnosti_idx` (`idDjeca` ASC, `idAktivnosti` ASC)  COMMENT '',
+  INDEX `fk_Aktivnosti_idx` (`idAktivnosti` ASC)  COMMENT '',
+  INDEX `fk_Djeca_idx` (`idDjeca` ASC)  COMMENT '',
   CONSTRAINT `fk_Djeca`
-    FOREIGN KEY ()
-    REFERENCES `mydb`.`Djeca` ()
+    FOREIGN KEY (`idDjeca`)
+    REFERENCES `mydb`.`Djeca` (`idDjeca`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Aktivnosti`
-    FOREIGN KEY (`idDjeca` , `idAktivnosti`)
-    REFERENCES `mydb`.`Aktivnosti` (`idAktivnosti` , `idAktivnosti`)
+    FOREIGN KEY (`idAktivnosti`)
+    REFERENCES `mydb`.`Aktivnosti` (`idAktivnosti`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
