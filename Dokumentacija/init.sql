df399a2172da3f9e0fc8053e0c9a14e9e1c58fb8
TheAdnan <adnankicin92@gmail.com>
2016-05-08 22:13:39 +0200

    init.sql v6

--- Dokumentacija/init.sql
+++ Dokumentacija/init.sql
@@ -19,9 +19,9 @@ USE `tim9` ;
 -- -----------------------------------------------------
 -- Table `tim9`.`Grupe`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `tim9`.`Grupe` ;
+DROP TABLE IF EXISTS `tim9`.`Grupa` ;
 
-CREATE TABLE IF NOT EXISTS `tim9`.`Grupe` (
+CREATE TABLE IF NOT EXISTS `tim9`.`Grupa` (
   `idGrupe` INT NOT NULL COMMENT '',
   `naziv` VARCHAR(45) NULL COMMENT '',
   `RedniBroj` INT(11) NULL COMMENT '',
@@ -33,10 +33,10 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `tim9`.`Djeca`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `tim9`.`Djeca` ;
+DROP TABLE IF EXISTS `tim9`.`Dijete` ;
 
-CREATE TABLE IF NOT EXISTS `tim9`.`Djeca` (
-  `idDjeca` INT NOT NULL COMMENT '',
+CREATE TABLE IF NOT EXISTS `tim9`.`Dijete` (
+  `idDijete` INT NOT NULL COMMENT '',
   `Ime` VARCHAR(255) NULL COMMENT '',
   `Prezime` VARCHAR(255) NULL COMMENT '',
   `DatumRodjenja` VARCHAR(255) NULL COMMENT '',
@@ -47,11 +47,11 @@ CREATE TABLE IF NOT EXISTS `tim9`.`Djeca` (
   `DatumUpisa` DATE NULL COMMENT '',
   `DatumIsteka` DATE NULL COMMENT '',
   `idGrupe` INT NULL COMMENT '',
-  PRIMARY KEY (`idDjeca`)  COMMENT '',
+  PRIMARY KEY (`idDijete`)  COMMENT '',
   INDEX `fk_grupe_idx` (`idGrupe` ASC)  COMMENT '',
   CONSTRAINT `fk_grupe`
     FOREIGN KEY (`idGrupe`)
-    REFERENCES `tim9`.`Grupe` (`idGrupe`)
+    REFERENCES `tim9`.`Grupa` (`idGrupe`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -60,9 +60,9 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `tim9`.`Korisnici`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `tim9`.`Korisnici` ;
+DROP TABLE IF EXISTS `tim9`.`Korisnik` ;
 
-CREATE TABLE IF NOT EXISTS `tim9`.`Korisnici` (
+CREATE TABLE IF NOT EXISTS `tim9`.`Korisnik` (
   `idKorisnika` INT NOT NULL COMMENT '',
   `Ime` VARCHAR(255) NULL COMMENT '',
   `Prezime` VARCHAR(255) NULL COMMENT '',
@@ -77,20 +77,20 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `tim9`.`Uplate`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `tim9`.`Uplate` ;
+DROP TABLE IF EXISTS `tim9`.`Uplata` ;
 
-CREATE TABLE IF NOT EXISTS `tim9`.`Uplate` (
+CREATE TABLE IF NOT EXISTS `tim9`.`Uplata` (
   `idUplate` INT NOT NULL COMMENT '',
   `DatumUplate` DATE NULL COMMENT '',
   `VisinaUplate` DOUBLE NULL COMMENT '',
-  `idDjeca` INT NULL COMMENT '',
+  `idDijete` INT NULL COMMENT '',
   `zaMjesec` INT NULL COMMENT '',
   `zaGodinu` INT NULL COMMENT '',
   PRIMARY KEY (`idUplate`)  COMMENT '',
-  INDEX `idDjeca_idx` (`idDjeca` ASC)  COMMENT '',
-  CONSTRAINT `idDjeca`
-    FOREIGN KEY (`idDjeca`)
-    REFERENCES `tim9`.`Djeca` (`idDjeca`)
+  INDEX `idDjeca_idx` (`idDijete` ASC)  COMMENT '',
+  CONSTRAINT `idDijete`
+    FOREIGN KEY (`idDijete`)
+    REFERENCES `tim9`.`Dijete` (`idDijete`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -99,20 +99,20 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `tim9`.`Vaspitaci`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `tim9`.`Vaspitaci` ;
+DROP TABLE IF EXISTS `tim9`.`Vaspitac` ;
 
-CREATE TABLE IF NOT EXISTS `tim9`.`Vaspitaci` (
-  `idVaspitaci` INT NOT NULL COMMENT '',
+CREATE TABLE IF NOT EXISTS `tim9`.`Vaspitac` (
+  `idVaspitac` INT NOT NULL COMMENT '',
   `Ime` VARCHAR(255) NULL COMMENT '',
   `Prezime` VARCHAR(255) NULL COMMENT '',
   `BrojTelefona` VARCHAR(255) NULL COMMENT '',
   `AdresaPrebivalista` VARCHAR(255) NULL COMMENT '',
   `idGrupe` INT NULL COMMENT '',
-  PRIMARY KEY (`idVaspitaci`)  COMMENT '',
+  PRIMARY KEY (`idVaspitac`)  COMMENT '',
   INDEX `GrupeID_idx` (`idGrupe` ASC)  COMMENT '',
   CONSTRAINT `GrupeID`
     FOREIGN KEY (`idGrupe`)
-    REFERENCES `tim9`.`Grupe` (`idGrupe`)
+    REFERENCES `tim9`.`Grupa` (`idGrupe`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -121,9 +121,9 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `tim9`.`Aktivnosti`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `tim9`.`Aktivnosti` ;
+DROP TABLE IF EXISTS `tim9`.`Aktivnost` ;
 
-CREATE TABLE IF NOT EXISTS `tim9`.`Aktivnosti` (
+CREATE TABLE IF NOT EXISTS `tim9`.`Aktivnost` (
   `idAktivnosti` INT NOT NULL COMMENT '',
   `Naziv` VARCHAR(255) NULL COMMENT '',
   `BrojDjece` INT NULL COMMENT '',
@@ -138,18 +138,18 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `tim9`.`AktivnostiDjeca` ;
 
 CREATE TABLE IF NOT EXISTS `tim9`.`AktivnostiDjeca` (
-  `idDjeca` INT NOT NULL COMMENT '',
+  `idDijete` INT NOT NULL COMMENT '',
   `idAktivnosti` INT NOT NULL COMMENT '',
   INDEX `fk_Aktivnosti_idx` (`idAktivnosti` ASC)  COMMENT '',
-  INDEX `fk_Djeca_idx` (`idDjeca` ASC)  COMMENT '',
+  INDEX `fk_Djeca_idx` (`idDijete` ASC)  COMMENT '',
   CONSTRAINT `fk_Djeca`
-    FOREIGN KEY (`idDjeca`)
-    REFERENCES `tim9`.`Djeca` (`idDjeca`)
+    FOREIGN KEY (`idDijete`)
+    REFERENCES `tim9`.`Dijete` (`idDijete`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Aktivnosti`
     FOREIGN KEY (`idAktivnosti`)
-    REFERENCES `tim9`.`Aktivnosti` (`idAktivnosti`)
+    REFERENCES `tim9`.`Aktivnost` (`idAktivnosti`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -162,14 +162,14 @@ DROP TABLE IF EXISTS `tim9`.`Zaduzenja` ;
 
 CREATE TABLE IF NOT EXISTS `tim9`.`Zaduzenja` (
   `idZaduzenja` INT NOT NULL COMMENT '',
-  `idDjeca` INT NULL COMMENT '',
+  `idDijete` INT NULL COMMENT '',
   `mjesec` VARCHAR(45) NULL COMMENT '',
   `godina` INT NULL COMMENT '',
   PRIMARY KEY (`idZaduzenja`)  COMMENT '',
-  INDEX `fk_djeca_1_idx` (`idDjeca` ASC)  COMMENT '',
+  INDEX `fk_djeca_1_idx` (`idDijete` ASC)  COMMENT '',
   CONSTRAINT `fk_djeca_1`
-    FOREIGN KEY (`idDjeca`)
-    REFERENCES `tim9`.`Djeca` (`idDjeca`)
+    FOREIGN KEY (`idDijete`)
+    REFERENCES `tim9`.`Dijete` (`idDijete`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -178,26 +178,26 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `tim9`.`Termini`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `tim9`.`Termini` ;
+DROP TABLE IF EXISTS `tim9`.`Termin` ;
 
-CREATE TABLE IF NOT EXISTS `tim9`.`Termini` (
-  `idTermini` INT NOT NULL COMMENT '',
+CREATE TABLE IF NOT EXISTS `tim9`.`Termin` (
+  `idTermin` INT NOT NULL COMMENT '',
   `idAktivnosti` INT NULL COMMENT '',
   `idGrupe` INT NULL COMMENT '',
   `vrijemePocetka` VARCHAR(45) NULL COMMENT '',
   `vrijemeZavrsetka` VARCHAR(45) NULL COMMENT '',
   `dan` VARCHAR(45) NULL COMMENT '',
-  PRIMARY KEY (`idTermini`)  COMMENT '',
+  PRIMARY KEY (`idTermin`)  COMMENT '',
   INDEX `fk_aktivnosti_1_idx` (`idAktivnosti` ASC)  COMMENT '',
   INDEX `fk_grupe_1_idx` (`idGrupe` ASC)  COMMENT '',
   CONSTRAINT `fk_aktivnosti_1`
     FOREIGN KEY (`idAktivnosti`)
-    REFERENCES `tim9`.`Aktivnosti` (`idAktivnosti`)
+    REFERENCES `tim9`.`Aktivnost` (`idAktivnosti`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_grupe_1`
     FOREIGN KEY (`idGrupe`)
-    REFERENCES `tim9`.`Grupe` (`idGrupe`)
+    REFERENCES `tim9`.`Grupa` (`idGrupe`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
