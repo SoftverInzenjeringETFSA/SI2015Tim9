5776d4505fc39a7a2ff1b5e63918a13256feebf2
Nejra Pasic <=>
2016-05-12 15:12:13 +0200

    Testovi drugi put

---
+++ vrtic/vrtic-app/src/test/java/app/vrtic/TestDijeteServis.java
@@ -0,0 +1,15 @@
+package app.vrtic;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestDijeteServis {
+
+	
+	@Test
+	public void testDijeteServisKonst() {
+		
+	}
+
+}
