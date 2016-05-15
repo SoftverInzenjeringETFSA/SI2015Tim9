579fa4e67d89ff3c326cb9182014f5580e5d0936
Npasic1 <npasic1@etf.unsa.ba>
2016-05-15 21:14:54 +0200

    Add files via upload

--- vrtic/vrtic-app/src/test/java/app/vrtic/UplataServistTest.java
+++ vrtic/vrtic-app/src/test/java/app/vrtic/UplataServistTest.java
@@ -37,90 +37,92 @@ public class UplataServistTest {
     static Session sesija = HibernateUtil.getSessionFactory().openSession();
     static UplataServis us = new UplataServis(sesija);
     @BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		
-	    	AktivnostServis as = new AktivnostServis(sesija);
-	    	DijeteServis ds = new DijeteServis(sesija);
-	    	GrupaServis gs = new GrupaServis(sesija);
-	    	KorisnikServis ks = new KorisnikServis(sesija);
-	    	TerminServis ts = new TerminServis(sesija);
-	    	UplataServis us = new UplataServis(sesija);
-	    	VaspitacServis vs = new VaspitacServis(sesija);
-	    	ZaduzenjeServis zs = new ZaduzenjeServis(sesija);
-	    	
-	    	Set<Termin> set_t = new HashSet<Termin>();
-	    	Set<Aktivnostidjeca> set_ad = new HashSet<Aktivnostidjeca>();
-	    	
-	    	
-	    	
-	    	Set<Vaspitac> set_v = new HashSet<Vaspitac>();
-	    	Set<Dijete> set_d = new HashSet<Dijete>();
-	    	Calendar c = Calendar.getInstance();
-			Date datum = c.getTime();
-	    	Grupa g = new Grupa("Grupa", 12, 13, set_v, set_d, set_t);
-	    	
-	    	Vaspitac v= new Vaspitac(g, "Ime", "Przime", "033225883", "Adresa");
-	    	set_v.add(v);
-	    	g.setVaspitacs(set_v);
-	    	Aktivnost a = new Aktivnost("Aktivnost", 12, 12, set_t, set_ad);
-	    	Termin t = new Termin(a, g, "17:00", "20:00", "ponedjeljak");
-	    	t.setIdTermin(1);
-	    	set_t.add(t);
-	    	g.setTermins(set_t);
-	    	a.setTermins(set_t);
-	    	
-	    	
-	       	Set<Zaduzenja> set_z = new HashSet<Zaduzenja>();
-	       	
-	    	Set<Uplata> set_u = new HashSet<Uplata>();
-	    	
-	    	Dijete d = new Dijete(g, "ImeDjeteta", "PrezimeDjeteta", "2011-12-12", "Adresa", "Roditelj", "033225883", "Prezime",
-	    			datum, datum, set_u, set_ad, set_z);
-	    	Zaduzenja z = new Zaduzenja(d, "januar", 2016);
-	    	Uplata u = new Uplata(d, datum, 100.5, 2, 2015);
-	    	set_u.add(u);
-	    	d.setUplatas(set_u);
-	    	set_z.add(z);
-	    	d.setZaduzenjas(set_z);
-	    	Korisnik k = new Korisnik("ImeKorisnika", "PrezimeKorisnika", "usename", "sifra", "direktor", "033225883");
-	    	k.setIdKorisnika(1);
-	    	u.setIdUplate(1);
-	    	z.setIdZaduzenja(1);
-	    	d.setIdDijete(1);
-	    	g.setIdGrupe(1);
-	    	v.setIdVaspitac(1);
-	    	a.setIdAktivnosti(1);
-	    	t.setIdTermin(1);
-	    	
-	    	  
- 
-	    	if(as.pretragaPoIDu(1) == null){
-	    		as.dodajAktivnost(a);
-	    	}
-	    	if(gs.PretragaPoIDu(1) == null){
-		    	gs.dodajGrupu(g);
-		    	}
-	    	if(ts.vratiTerminPoId(1) == null){
-		       	ts.dodajTermin(t);
-		   
-	    	}
-	    	if(ks.dajKorisnika(1) == null){
-	    	ks.kreirajKorisnika(k);
-	    	}
-	    	
-	    	/*if(== null){
-	    	us.evidentirajUplatu(u);
-	    	}*/
-	    	
-	    	if(vs.nadji(1) == null){
-		    	vs.evidentiraj(v);
-		    	}  
-	    	if(ds.nadji(1) == null){
-		    	ds.evidentiraj(d);
-		    	}
-		    	
-	  
-	}
+  	public static void setUpBeforeTest() throws Exception {
+  		
+  	    	AktivnostServis as = new AktivnostServis(sesija);
+  	    	DijeteServis ds = new DijeteServis(sesija);
+  	    	GrupaServis gs = new GrupaServis(sesija);
+  	    	KorisnikServis ks = new KorisnikServis(sesija);
+  	    	TerminServis ts = new TerminServis(sesija);
+  	    	UplataServis us = new UplataServis(sesija);
+  	    	VaspitacServis vs = new VaspitacServis(sesija);
+  	    	ZaduzenjeServis zs = new ZaduzenjeServis(sesija);
+  	    	Aktivnost a = new Aktivnost();
+  	    	Grupa g = new Grupa();
+  	    	Termin t = new Termin();
+  	    	Korisnik k = new Korisnik();
+  	    	Vaspitac v = new Vaspitac();
+  	    	Dijete d = new Dijete();
+  	    	Calendar c = Calendar.getInstance();
+  			Date datum = c.getTime();
+  	    	if(as.pretragaPoIDu(1) == null){
+  	    		
+  	    		a.setBrojDjece(10);
+  	    		a.setCijena(10);
+  	    		a.setIdAktivnosti(1);
+  	    		a.setNaziv("Aktivnost");
+  	    		
+  	    		as.dodajAktivnost(a);
+  	    	}
+  	    	if(gs.PretragaPoIDu(1) == null){
+  	    		
+  	    		g.setIdGrupe(1);
+  	    		g.setKapacitet(10);
+  	    		g.setNaziv("Grupa");
+  	    		g.setRedniBroj(2);
+  	    		
+  		    	gs.dodajGrupu(g);
+  		    	}
+  	    	if(ts.vratiTerminPoId(1) == null){
+  	    		t.setIdTermin(1);
+  	    		t.setAktivnost(a);
+  	    		t.setDan("Ponedjeljak");
+  	    		t.setGrupa(g);
+  		       	ts.dodajTermin(t);
+  		   
+  	    	}
+  	    	if(ks.dajKorisnika(1) == null){
+  	    		k.setBrojTelefona("033225883");
+  	    		k.setIdKorisnika(1);
+  	    		k.setIme("Korisnik");
+  	    		k.setKorisnickoIme("username");
+  	    		k.setPrezime("Prezime");
+  	    		k.setPrivilegije("direktor");
+  	    		k.setSifra("sifra");
+  	    	ks.kreirajKorisnika(k);
+  	    	}
+  	    	
+  	    	/*if(== null){
+  	    	us.evidentirajUplatu(u);
+  	    	}*/
+  	    	
+  	    	if(vs.nadji(1) == null){
+  	    		v.setAdresaPrebivalista("adresa");
+  	    		v.setBrojTelefona("033225883");
+  	    		v.setGrupa(g);
+  	    		v.setIdVaspitac(1);
+  	    		v.setIme("Ime");
+  	    		v.setPrezime("Prezime");
+  		    	vs.evidentiraj(v);
+  		    	}  
+  	    	if(ds.nadji(1) == null){
+  	    		d.setAdresaPrebivalista("Adresa");
+  	    		d.setBrojTelefona("033225883");
+  	    		d.setDatumIsteka(datum);
+  	    		d.setDatumRodjenja("2000-12-12");
+  	    		d.setDatumUpisa(datum);
+  	    		d.setGrupa(g);
+  	    		d.setIdDijete(1);
+  	    		d.setIme("Dijet");
+  	    		d.setImeRoditelja("roditelj");
+  	    		d.setPrezime("Prezime");
+  	    		d.setPrezimeRoditelja("Prezime");
+  	    		
+  		    	ds.evidentiraj(d);
+  		    	}
+  		    	
+  	  
+  	}
 	@Test
 
 	public void EvidentirajUplatuTest() throws Exception{
